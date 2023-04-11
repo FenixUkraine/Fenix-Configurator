d9ee5ad9badff618801fbe433924a3d425cb10d6
Denys Batrak <baden.i.ua@gmail.com>
2023-04-12 01:59:07 +0300

    Initial commit.

---
+++ src/boot/ble.js
@@ -0,0 +1,43 @@
+// I am sure, here is not best place for this code, but I don't know where to put it
+import { boot } from "quasar/wrappers";
+
+class BLE {
+  constructor() {
+    this.PRIMARY_SERVICE = "0de1fa51-7504-44e5-95f4-9e791515f55a";
+    this.CHARACTERISTIC_STATUS = "de1fa512-7504-44e5-95f4-9e791515f55a";
+    this.device = null;
+    this.server = null;
+    this.service = null;
+    this.characteristic = null;
+  }
+
+  // API
+  async connect() {
+    console.log("Connecting...", this);
+    this.device = await navigator.bluetooth.requestDevice({
+      filters: [{ services: [this.PRIMARY_SERVICE] }],
+    });
+    this.server = await this.device.gatt.connect();
+    this.service = await this.server.getPrimaryService(this.PRIMARY_SERVICE);
+    this.characteristic = await this.service.getCharacteristic(
+      this.CHARACTERISTIC_STATUS
+    );
+  }
+
+  async disconnect() {
+    if (this.device) {
+      await this.device.gatt.disconnect();
+    }
+  }
+
+  async write(value) {
+    await this.characteristic.writeValue(value);
+  }
+}
+
+console.log("BLE boot script loaded");
+
+export default boot(({ app }) => {
+  console.log("BLE boot script started");
+  app.config.globalProperties.$ble = new BLE();
+});
