c09817dab57f98d4ef091d13b97a4535617e3976
dr008 <yasins.yar@gmail.com>
2023-04-16 17:25:28 +0300

    settings page template

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,17 @@
 <template>
   <q-page class="flex flex-center">
+    Counter: {{ store.count }}
+    <q-btn @click="store.increment">Increment</q-btn>
     <q-btn v-on:click="$ble.connect()">Connect BLE</q-btn>
   </q-page>
 </template>
 
 <script setup>
+import { useCounterStore } from "../stores/example-store";
+const store = useCounterStore();
+</script>
+
+<script>
 import { defineComponent } from "vue";
 
 export default defineComponent({
